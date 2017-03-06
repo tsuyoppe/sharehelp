1589aede81ea6ddbc5d8e951a76aa0c539e4cdbd
Tsuyoshi.m <tsuyopon012345@gmail.com>
2017-03-06 20:23:03 +0000

    friendsnandane

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Category.create([
+        {category: 'iPhone'},
+        {category: 'Android'},
+        {category: 'WebD'},
+        {category: 'WebS'},
+        {category: 'Movie'},
+        {category: 'IoT'},
+])
+    
